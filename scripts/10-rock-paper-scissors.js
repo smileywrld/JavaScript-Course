6e815374d87a3b4ecba3154bcc46c67c3cded391
smileywrld <ismaileyyunusa@gmail.com>
2025-02-21 21:21:06 +0100

    fix: correct typo in computer move selection for Rock Paper Scissors game

--- scripts/10-rock-paper-scissors.js
+++ scripts/10-rock-paper-scissors.js
@@ -78,7 +78,7 @@ function pickcomputerMove() {
 	if (randomNumber >= 0 && randomNumber < 1 / 3) {
 		computerMove = "rock";
 	} else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-		computerMove = "aper";
+		computerMove = "paper";
 	} else if (randomNumber >= 2 / 3 && randomNumber < 1) {
 		computerMove = "scissors";
 	}

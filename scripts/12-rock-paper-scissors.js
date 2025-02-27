cd3e8a88ff854cbeed90b17f4ec0d3e6763fff68
smileywrld <ismaileyyunusa@gmail.com>
2025-02-27 22:26:46 +0100

    add: implement keyboard controls for rock-paper-scissors game

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -68,6 +68,16 @@ document.querySelector(".js-auto-play-button").addEventListener("click", () => {
 
 // Used event listener instead of onclick attribute
 
+document.body.addEventListener("keydown", (event) => {
+	if (event.key === "r") {
+		playGame("rock");
+	} else if (event.key === "p") {
+		playGame("paper");
+	} else if (event.key === "s") {
+		playGame("scissors");
+	}
+});
+
 function playGame(playerMove) {
 	playerMove = playerMove.toLowerCase();
 	const computerMove = pickcomputerMove();
